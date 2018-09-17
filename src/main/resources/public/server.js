ad41c0c078a6a9cb63e50a5143ddddf12582b7ad
OlgaNika <olga.k.nikitenko@gmail.com>
2018-09-17 13:28:22 +0300

    minor changes

--- src/main/resources/public/server.js
+++ src/main/resources/public/server.js
@@ -1,3 +1,7 @@
+//run node web server to speedup development
+//npm install connect serve-static
+//node server.js
+
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(8004, function(){
