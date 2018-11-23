e514ace60ddedac74714fbcf339e085f56e6569a
OlgaNika <olga.k.nikitenko@gmail.com>
2018-11-23 22:53:03 +0300

    comments removed

--- src/main/java/restapp/RestApplication.java
+++ src/main/java/restapp/RestApplication.java
@@ -11,13 +11,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class RestApplication  {
 
     private static final Logger logger = LogManager.getLogger(RestApplication.class);
-
-
-
+    
     //TODO 1)fix issue with time zone = post new expence created with prev date - fixed with WA +4 hours added
-    // the issue with TZ sync - needs to be fixed permanantly
-//TODO 2) add user's authorisation - create default user admin/3
-    //now particular user is able to login - this is fine for initial version
 
     //rest api supports this via GET /expence call.
     public static void main(String[] args) {
